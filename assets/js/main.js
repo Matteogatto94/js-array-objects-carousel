4e41426ca5e3fe031f4e0eb57c4b425f2a43e129
Matteogatto94 <cosenza94@live.it>
2022-10-21 20:47:25 +0200

    add array images, button and container

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,3 +17,30 @@ Aggiungere funzionalità di autoplay: dopo un certo periodo di tempo (3 secondi)
 BONUS 3  (opzionale):
 Aggiungere bottoni di start/stop e di inversione del meccanismo di autoplay.*/
 
+const imagesArray = [
+    {
+        image: './assets/img/01.webp',
+        title: 'Marvel\'s Spiderman Miles Morale',
+        description: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+    }, 
+    {
+        image: './assets/img/02.webp',
+        title: 'Ratchet & Clank: Rift Apart',
+        description: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+    }, 
+    {
+        image: './assets/img/03.webp',
+        title: 'Fortnite',
+        description: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+    }, 
+    {
+        image: './assets/img/04.webp',
+        title: 'Stray',
+        description: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+    }, 
+    {
+        image: './assets/img/05.webp',
+        title: "Marvel's Avengers",
+        description: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+    }
+]
