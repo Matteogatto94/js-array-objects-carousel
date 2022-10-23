1a98bb5fe5fce0e0ca407af8b61f99c74d2abcf3
Matteogatto94 <cosenza94@live.it>
2022-10-23 23:12:50 +0200

    add carousel

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,46 +1,96 @@
-/*Consegna:
-Dato un array di oggetti letterali con:
-url dell’immagine
-titolo
-descrizione Creare un carosello come nella foto allegata.
-Milestone 0:
-Come nel primo carosello realizzato, focalizziamoci prima sulla creazione del markup statico: costruiamo il container e inseriamo l'immagine grande in modo da poter stilare lo slider.
-Milestone 1:
-Ora rimuoviamo i contenuti statici e usiamo l’array di oggetti letterali per popolare dinamicamente il carosello.
-Al click dell'utente sulle frecce verso sinistra o destra, l'immagine attiva diventerà visibile e dovremo aggiungervi titolo e testo.
-Milestone 2:
-Aggiungere il ciclo infinito del carosello. Ovvero se la miniatura attiva è la prima e l'utente clicca la freccia verso destra, la miniatura che deve attivarsi sarà l'ultima e viceversa per l'ultima miniatura se l'utente clicca la freccia verso sinistra.
-BONUS 1 (opzionale):
-Aggiungere le thumbnails (sottoforma di miniatura) ed al click attivare l’immagine corrispondente.
-BONUS 2  (opzionale):
-Aggiungere funzionalità di autoplay: dopo un certo periodo di tempo (3 secondi) l’immagine attiva dovrà cambiare alla successiva.
-BONUS 3  (opzionale):
-Aggiungere bottoni di start/stop e di inversione del meccanismo di autoplay.*/
+// Consegna:
+// Dato un array di oggetti letterali con:
+// url dell’immagine
+// titolo
+// descrizione
 
-const imagesArray = [
+// Creare un carosello come nella foto allegata.
+
+// Milestone 0:
+// Come nel primo carosello realizzato, focalizziamoci prima sulla creazione del markup statico: costruiamo il container e inseriamo l'immagine grande in modo da poter stilare lo slider.
+
+const images = [
     {
-        image: './assets/img/01.webp',
-        title: 'Marvel\'s Spiderman Miles Morale',
-        description: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-    }, 
+        image:'img/01.webp',
+        title: 'Marvel Spiderman Miles Morales',
+        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+    },
     {
-        image: './assets/img/02.webp',
+        image:'img/02.webp',
         title: 'Ratchet & Clank: Rift Apart',
-        description: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-    }, 
+        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+    },
     {
-        image: './assets/img/03.webp',
+        image:'img/03.webp',
         title: 'Fortnite',
-        description: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-    }, 
+        text: 'Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.',
+    },
     {
-        image: './assets/img/04.webp',
+        image:'img/04.webp',
         title: 'Stray',
-        description: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-    }, 
+        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+    },
     {
-        image: './assets/img/05.webp',
-        title: "Marvel's Avengers",
-        description: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+        image:'img/05.webp',
+        title: "Marvel Avengers",
+        text: 'Marvel Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
     }
-]
+  ];
+  
+  
+  const slidesElement = document.querySelector('.slides'); 
+  const nextElement = document.querySelector('.next'); 
+  const prevElement = document.querySelector('.prev'); 
+  
+  
+  let activeImage = 0;
+
+  
+  for (let i = 0; i < images.length; i++) {
+    const slideUrl = images[i].image;
+    const titolo = images[i].title;
+    const testo = images[i].text;
+
+    const imgMarkup = 
+    `<div class="card ${i === activeImage ? 'active' : ''}">
+        <img class="img-fluid " src="./assets/${slideUrl}" alt= "">
+        <div class="body_card">
+            <h1 class="text-center">${titolo}</h1>
+            <p class="text-center">${testo}</p>
+        </div>
+    </div>`;
+    slidesElement.insertAdjacentHTML('beforeend', imgMarkup);
+  }
+  
+ 
+  nextElement.addEventListener('click', function () {
+  
+  
+    const activeSlideElement = document.querySelector('.slides > .card.active')
+    activeSlideElement.classList.remove('active');
+  
+    activeImage++ 
+    if (activeImage === 5) {
+        activeImage = 0;
+    }
+
+    const allSlides = document.getElementsByClassName('card')
+    const nextSlideElement = allSlides[activeImage];
+    nextSlideElement.classList.add('active')
+  
+  });
+  
+  prevElement.addEventListener('click', function () {
+    
+    const activeSlideElement = document.querySelector('.slides > .card.active')
+    activeSlideElement.classList.remove('active');
+
+    if (activeImage == 0) {
+        activeImage = 5;
+    }
+
+    activeImage-- 
+    const allSlides = document.getElementsByClassName('card')
+    const nextSlideElement = allSlides[activeImage];
+    nextSlideElement.classList.add('active')
+  });
